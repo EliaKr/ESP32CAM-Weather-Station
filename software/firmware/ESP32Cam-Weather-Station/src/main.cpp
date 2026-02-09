ed3881cf406c25822025d0c4a25bc3c3a32e1714
EliaKr <ikrigos@gmail.com>
2026-02-09 22:31:54 +0200

    [WIP] Added some testing code

--- software/firmware/ESP32Cam-Weather-Station/src/main.cpp
+++ software/firmware/ESP32Cam-Weather-Station/src/main.cpp
@@ -1,24 +1,54 @@
+#include <Arduino.h>
 #include <WiFi.h>
 #include <WebServer.h>
 #include "web/index_html.h"
 
+// Define which mode to use (comment out one)
+//#define USE_WIFI_MODE
+#define USE_SOFTAP_MODE
+
 // WiFi credentials
-const char* ssid = "SSID";
-const char* password = "PWD";
+const char* ssid = "-";
+const char* password = "-";
+
+// SoftAP credentials
+const char* ap_ssid = "ESP32-Weather";
+const char* ap_password = "12345678";
+
+// LED pin
+#define LED_PIN 4
 
 // Create web server on port 80
 WebServer server(80);
 
-// Forward declaration - ADD THIS LINE
+// Forward declaration
 void handleRoot();
 
 void setup() {
+  pinMode(LED_PIN, OUTPUT);
+  digitalWrite(LED_PIN, LOW);
+  
+#ifdef USE_WIFI_MODE
   // Connect to WiFi
+  WiFi.mode(WIFI_STA);
   WiFi.begin(ssid, password);
   
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
   }
+  digitalWrite(LED_PIN, HIGH);
+  delay(500);
+  digitalWrite(LED_PIN, LOW);
+  
+#elif defined(USE_SOFTAP_MODE)
+  // Start SoftAP mode
+  WiFi.mode(WIFI_AP);
+  WiFi.softAP(ap_ssid, ap_password);
+  digitalWrite(LED_PIN, HIGH);
+  delay(500);
+  digitalWrite(LED_PIN, LOW);
+  
+#endif
   
   // Configure web server routes
   server.on("/", handleRoot);
@@ -28,11 +58,9 @@ void setup() {
 }
 
 void loop() {
-  // Handle incoming client requests
   server.handleClient();
 }
 
 void handleRoot() {
-  // Serve the HTML page from the separate file
   server.send(200, "text/html", index_html);
 }
